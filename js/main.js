80314ab391e6bfbd14ded78cfe3cae0ec9fc2a98
meeglos <miglos@outlook.es>
2015-11-15 00:01:58 +0000

    Index, main css and js files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* 
+* @Author: 				miguel
+* @Date:   				2015-11-11 02:02:23
+* @Email: 				miglos@outlook.es
+* @Last Modified by:	miguel
+* @Last Modified time:	2015-11-12 23:41:26
+*/
+
+'use strict';
